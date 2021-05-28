7208a21bcc41d9ed2f1975d0a106ce17c881cf50
matheusgomes28 <matheusgarcia28@gmail.com>
2021-05-28 07:25:44 +0100

    Layed out the benchmarks side by side.

--- benchmarks/benchmarks.cpp
+++ benchmarks/benchmarks.cpp
@@ -27,21 +27,6 @@ static void BM_StandardImplementation(benchmark::State& state)
 	}
 }
 
-auto benchmark1 = BM_StandardImplementation<-10, 10, 100>;
-BENCHMARK(benchmark1);
-
-auto benchmark2 = BM_StandardImplementation<-10, 10, 1000>;
-BENCHMARK(benchmark2);
-
-auto benchmark3 = BM_StandardImplementation<-15, 15, 10000>;
-BENCHMARK(benchmark3);
-
-auto benchmark4 = BM_StandardImplementation<-15, 15, 20000>;
-BENCHMARK(benchmark4);
-
-auto benchmark5 = BM_StandardImplementation<-20, 20, 50000>;
-BENCHMARK(benchmark5);
-
 template <int a, int b, std::size_t n>
 static void BM_EigenImplementation(benchmark::State& state)
 {
@@ -52,17 +37,33 @@ static void BM_EigenImplementation(benchmark::State& state)
 	}
 }
 
+
+auto bm_standard_traps10x10x100= BM_StandardImplementation<-10, 10, 100>;
+BENCHMARK(bm_standard_traps10x10x100);
+
 auto bm_eigen10x10x100 = BM_EigenImplementation<-10, 10, 100>;
 BENCHMARK(bm_eigen10x10x100);
+
+auto bm_standard_traps10x10x1000 = BM_StandardImplementation<-10, 10, 1000>;
+BENCHMARK(bm_standard_traps10x10x1000);
 
 auto bm_eigen10x10x1000 = BM_EigenImplementation<-10, 10, 1000>;
 BENCHMARK(bm_eigen10x10x1000);
 
+auto bm_standard_traps15x15x10000 = BM_StandardImplementation<-15, 15, 10000>;
+BENCHMARK(bm_standard_traps15x15x10000);
+
 auto bm_eigen15x15x10000 = BM_EigenImplementation<-15, 15, 10000>;
 BENCHMARK(bm_eigen15x15x10000);
 
+auto bm_standard_traps15x15x20000 = BM_StandardImplementation<-15, 15, 20000>;
+BENCHMARK(bm_standard_traps15x15x20000);
+
 auto bm_eigen15x15x20000 = BM_EigenImplementation<-15, 15, 20000>;
 BENCHMARK(bm_eigen15x15x20000);
+
+auto bm_standard_traps20x20x50000= BM_StandardImplementation<-20, 20, 50000>;
+BENCHMARK(bm_standard_traps20x20x50000);
 
 auto bm_eigen20x20x50000 = BM_EigenImplementation<-20, 20, 50000>;
 BENCHMARK(bm_eigen20x20x50000);
